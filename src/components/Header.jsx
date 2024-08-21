964861343134a881efa6b0fc1b209a9c3428cebf
Michael McGallagly <mcgallagly@gmail.com>
2024-08-21 01:21:47 +0100

    Sidebar logic
    added in sidebar logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,13 @@
+import React, {useState} from 'react';
+
 export default function Header() {
+
+  const[isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <header className="flex items-center justify-between gap-4 p-7 border-b border-blue-200 shadow-lg shadow-blue-100 text-md sm:text-lg lg:text-xl  mb-4">
     <a href="/"><h1>Michael
