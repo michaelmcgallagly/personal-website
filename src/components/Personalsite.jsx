a51b6723333d1ec30425212a7c69954b105087cb
Michael McGallagly <mcgallagly@gmail.com>
2024-08-22 17:12:44 +0100

    Updated Links
    Updated links to my personal website project

--- src/components/Personalsite.jsx
+++ src/components/Personalsite.jsx
@@ -10,9 +10,9 @@ export default function Personalsite() {
         <h1 className="text-xl sm:text-2xl font-semibold mb-2">Personal Website</h1>
         <p>This website utilises the React.js library and the CSS framework Tailwind to create a sleek web design with interactive features. It is used to display myself and my projects.It is the site you are currently on! </p>
       
-       <a href="https://lonelyshop.netlify.app/"> <button className="bg-blue-500 text-white p-2 mt-2 mr-2 rounded hover:bg-blue-700 transition duration-300 ease-in-out">View Site</button>  </a> 
+       <a href="https://mcgallagly.netlify.app/"> <button className="bg-blue-500 text-white p-2 mt-2 mr-2 rounded hover:bg-blue-700 transition duration-300 ease-in-out">View Site</button>  </a> 
 
-       <a href="https://github.com/michaelmcgallagly/LonelyShop"> <button className="bg-gray-500 text-white p-2 mt-2 rounded hover:bg-gray-700 transition duration-300 ease-in-out">View Code</button>  </a> 
+       <a href="https://github.com/michaelmcgallagly/personal-website"> <button className="bg-gray-500 text-white p-2 mt-2 rounded hover:bg-gray-700 transition duration-300 ease-in-out">View Code</button>  </a> 
         </div>
 
       
